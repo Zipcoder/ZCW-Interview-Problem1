34583d41dc2d723baff99c5402858bf4253c704a
aaron <aaronherion111@gmail.com>
2017-11-27 10:05:15 -0500

    Aaron Herion Submission

--- src/main/java/io/zipcoder/Problem1.java
+++ src/main/java/io/zipcoder/Problem1.java
@@ -1,4 +1,28 @@
 package io.zipcoder;
 
+import java.util.HashMap;
+
 public class Problem1 {
+
+    String stringInput = "The Farmer went to the store to get 1 dollar’s worth of fertilizer";
+    char[] input = stringInput.toCharArray();
+    String result = "";
+
+    public String stringReplace() {
+
+        HashMap<Object, Object> replacerMap = new HashMap<Object, Object>();
+        replacerMap.put("f", 7);
+        replacerMap.put("s", "$");
+        replacerMap.put(1, "!");
+        replacerMap.put("a", "@");
+
+        for (HashMap.Entry<Object, Object> val : replacerMap.entrySet()) {
+
+            result = input.replace
+
+        }
+        return result;
+
+    }
+
 }
--- src/test/java/io/zipcoder/Problem1Test.java
+++ src/test/java/io/zipcoder/Problem1Test.java
@@ -1,4 +1,28 @@
 package io.zipcoder;
 
+import org.junit.Test;
+
 public class Problem1Test {
+
+    @Test
+    public void interviewProblemOneIterationTest() {
+
+        String input = "The Farmer went to the store to get 1 dollar’s worth of fertilizer";
+
+        String expected = "";
+        String actual = "";
+
+
+    }
+
+    @Test
+
+    public void interviewProblemOneRecursionTest() {
+
+        String input = "The Farmer went to the store to get 1 dollar’s worth of fertilizer";
+
+        String expected = "";
+        String actual = "";
+
+    }
 }
