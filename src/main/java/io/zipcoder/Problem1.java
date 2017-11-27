3faa6a8217a11b417ae77342f6b1dbfd23057db3
brian <brian@zipcoders-MacBook-Pro.local>
2017-11-27 10:05:44 -0500

    Saving progress

--- src/main/java/io/zipcoder/Problem1.java
+++ src/main/java/io/zipcoder/Problem1.java
@@ -1,4 +1,34 @@
 package io.zipcoder;
 
+import java.util.HashMap;
+
 public class Problem1 {
+
+    public String replaceByIteration(String input) {
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("f", "7");
+        map.put("s", "$");
+        map.put("1", "!");
+        map.put("a", "@");
+
+        String returnString = "";
+
+        for (int i = 0; i < input.length(); i++) {
+            String currentSubstring = input.substring(i, i + 1);
+
+            if (currentSubstring.equals("f") || currentSubstring.equals("s") || currentSubstring.equals("1") || currentSubstring.equals("a")) {
+                returnString += map.get(currentSubstring);
+            } else {
+                returnString += currentSubstring;
+            }
+        }
+        return returnString;
+    }
+
+    public String replaceByRecursion(String input) {
+        String returnString;
+        returnString = input.substring(0,1);
+        //returnString += replaceByRecursion(returnString);
+        return returnString;
+    }
 }
--- src/test/java/io/zipcoder/Problem1Test.java
+++ src/test/java/io/zipcoder/Problem1Test.java
@@ -1,4 +1,30 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem1Test {
+
+    @Test
+    public void replaceByIterationTest() {
+        String input = "The farmer went to the store to get 1 dollar's worth of fertilizer";
+
+        String actual = new Problem1().replaceByIteration(input);
+
+        String expected = "The 7@rmer went to the $tore to get ! doll@r'$ worth o7 7ertilizer";
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void replaceByRecursionTest() {
+        String input = "The farmer went to the store to get 1 dollar's worth of fertilizer";
+
+        String actual = new Problem1().replaceByRecursion(input);
+
+        String expected = "The 7@rmer went to the $tore to get ! doll@r'$ worth o7 7ertilizer";
+
+        Assert.assertEquals(expected, actual);
+    }
+
 }
