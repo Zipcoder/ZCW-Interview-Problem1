703465f268a32d764fffe1a3f1dc87df46548c49
rikeroleg <rikeroleg@gmail.com>
2017-11-27 10:04:35 -0500

    commit

--- src/test/java/io/zipcoder/Problem1Test.java
+++ src/test/java/io/zipcoder/Problem1Test.java
@@ -1,4 +1,19 @@
 package io.zipcoder;
 
+import org.junit.Test;
+
 public class Problem1Test {
+    String input = "The Farmer went to the store to get 1 dollar’s worth of fertilizer”;
+
+    @Test
+    public void iterationMethodTest(){
+        String expected = ;
+        String actual = ;
+    }
+
+    @Test
+    public void secondMethodName(){
+
+    }
+
 }
