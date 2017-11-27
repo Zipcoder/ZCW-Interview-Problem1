62a9b770536e6f18f6c6da67c6e9e404e216f7e0
vinceparis19 <vinceparis19@gmail.com>
2017-11-27 09:59:39 -0500

    firstTry/hardcoded

--- src/main/java/io/zipcoder/Problem1.java
+++ src/main/java/io/zipcoder/Problem1.java
@@ -1,4 +1,23 @@
 package io.zipcoder;
 
+import java.util.HashMap;
+import java.util.*;
+
 public class Problem1 {
+
+    public Problem1() {
+    }
+
+    public String kataSolution1(String str) {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("F", "7");
+        map.put("s", "$");
+        map.put("1", "!");
+        map.put("a", "@");
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            str = str.replace("${" + entry.getKey() + "}", entry.getValue());
+        }
+        System.out.println(str);
+        return str;
+    }
 }
