25c3047ff215eab4ff7a2d4ae63dec6256add9a0
gpatelos <greg.patselas@gmail.com>
2017-11-27 09:55:29 -0500

    one method test

--- src/test/java/io/zipcoder/Problem1Test.java
+++ src/test/java/io/zipcoder/Problem1Test.java
@@ -1,4 +1,33 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+import static javax.swing.UIManager.put;
+
 public class Problem1Test {
+
+
+
+    Problem1 problem1 = new Problem1();
+
+    @Test
+    public void findPostionInStringArray(){
+
+        String find = "F";
+        Integer expected = 4;
+        char convertedString = find.charAt(0) ;
+        Integer actual = problem1.findStringInStringArray( convertedString);
+
+        Assert.assertTrue(expected==actual);
+    }
+
+    
+
+
+
 }
