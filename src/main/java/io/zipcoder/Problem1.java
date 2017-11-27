09ba2494548aa66eb215c975e70ace0447b36569
katherine <katherine@zipcoders-MacBook-Pro.local>
2017-11-27 10:03:39 -0500

    finished

--- src/main/java/io/zipcoder/Problem1.java
+++ src/main/java/io/zipcoder/Problem1.java
@@ -1,4 +1,49 @@
 package io.zipcoder;
 
+import java.util.HashMap;
+
 public class Problem1 {
+
+    private HashMap<Character, Character> replaceChars = new HashMap<Character, Character>();
+
+    public Problem1() {
+        replaceChars.put('f', '7');
+        replaceChars.put('s', '$');
+        replaceChars.put('1', '!');
+        replaceChars.put('a', '@');
+    }
+
+    public String replaceCharsByIteration(String input) {
+        char[] allChars = input.toCharArray();
+        for(int i = 0; i < allChars.length; i++) {
+            char c = allChars[i];
+            if(replaceChars.containsKey(Character.toLowerCase(c))) {
+                allChars[i] = replaceChars.get(Character.toLowerCase(c));
+            }
+        }
+
+        return new String(allChars);
+    }
+
+    public String replaceCharsByRecursion(String input) {
+        char[] allChars = input.toCharArray();
+        for(char c : replaceChars.keySet()) {
+            int index = input.indexOf(Character.toLowerCase(c));
+            if(index > -1) {
+                allChars[index] = replaceChars.get(c);
+                input = new String(allChars);
+                replaceCharsByRecursion(input);
+            }
+            else {
+                index = input.indexOf(Character.toUpperCase(c));
+                if(index > -1) {
+                    allChars[index] = replaceChars.get(c);
+                    input = new String(allChars);
+                    replaceCharsByRecursion(input);
+                }
+            }
+        }
+        return input;
+    }
+
 }
