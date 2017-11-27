e915a0bcbeb5883d278f475fc6aa6ad6a37b701c
amyaim <amyaim99@gmail.com>
2017-11-27 10:05:29 -0500

    sad couldnt figure it out. but surely will get better

--- src/main/java/io/zipcoder/Problem1.java
+++ src/main/java/io/zipcoder/Problem1.java
@@ -1,4 +1,24 @@
 package io.zipcoder;
 
+import javax.swing.text.html.HTMLDocument;
+import java.util.*;
+
 public class Problem1 {
+
+    public void replaceKey(Map<Character, Long> stringMap) {
+
+
+        stringMap = new HashMap<Character , Long>();
+        Set<Character> keyValue = stringMap.keySet();
+        Map<Character, Long> replaced = new HashMap<Character, Long>();
+
+            boolean iterator = stringMap.containsKey(keyValue);
+            while (iterator) {
+
+                replaced.put(keyValue,  stringMap.values())
+            }
+        }
+    }
 }
+
+
--- src/test/java/io/zipcoder/Problem1Test.java
+++ src/test/java/io/zipcoder/Problem1Test.java
@@ -1,4 +1,14 @@
 package io.zipcoder;
 
+import org.junit.Test;
+
 public class Problem1Test {
+
+    Problem1 problem1 = new Problem1();
+    @Test
+    public void replaceKey() throws Exception {
+
+
+
+    }
 }
