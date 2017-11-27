8856c1de256a009c5547e729550de67a34c84fd1
andrea <andrea@zipcoders-MacBook-Pro-4.local>
2017-11-27 10:05:58 -0500

    ugh, realized halfway through to use maps

--- src/main/java/io/zipcoder/Problem1.java
+++ src/main/java/io/zipcoder/Problem1.java
@@ -1,4 +1,21 @@
 package io.zipcoder;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem1 {
+
+    String = "The Farmer went to the store to get 1 dollar's worth of fertilizer";
+
+    public static String iterationMethod(){
+        Map<Character, Character> newMap = new HashMap<Character, Character>();
+        for(Map.Entry<Character, Character> entry : map.entrySet())
+            newMap.put(entry.getKey().replace(), entry.getValue());
+        map = newMap;
+
+    }
+
+    public static String recursionMethod(){
+
+    }
 }
--- src/test/java/io/zipcoder/Problem1Test.java
+++ src/test/java/io/zipcoder/Problem1Test.java
@@ -1,4 +1,23 @@
 package io.zipcoder;
 
+import org.junit.Test;
+import org.junit.Assert;
+
 public class Problem1Test {
+
+    Problem1 problem1 = new Problem1();
+
+    @Test
+    public void iterationMethod(){
+        String expected = "The 7@rmer went to the $tore to get ! doll@r’$ worth of 7ertilizer";
+        //String actual = problem1.
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void recursionMethod(){
+        String expected = "The 7@rmer went to the $tore to get ! doll@r’$ worth of 7ertilizer";
+        //String actual = problem1.
+        Assert.assertEquals(expected,actual);
+    }
 }
