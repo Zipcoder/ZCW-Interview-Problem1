cfb2d544ee52d4c680d52dd7e2de761672b2e28a
wesley <wesley@zipcoders-MacBook-Pro-8.local>
2017-11-27 10:04:17 -0500

    problem 1

--- src/main/java/io/zipcoder/Problem1.java
+++ src/main/java/io/zipcoder/Problem1.java
@@ -1,4 +1,45 @@
 package io.zipcoder;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem1 {
+    private HashMap<Character,Character> changeMap = new HashMap<Character, Character>();
+    private String sentence;
+
+    public Problem1(String sentence, HashMap<Character,Character> changeMap){
+        this.sentence = sentence;
+        this.changeMap = changeMap;
+    }
+
+    public String changeWithLoop(){
+        StringBuilder newSentence = new StringBuilder();
+        for(int i = 0;i<sentence.length();i++){
+            Character currentCharacter = sentence.charAt(i);
+            if(changeMap.containsKey(Character.toLowerCase(currentCharacter)) ||
+                    changeMap.containsKey(Character.toUpperCase(currentCharacter))){
+                newSentence.append(changeMap.get(Character.toLowerCase(currentCharacter)));
+            }else{
+                newSentence.append(currentCharacter);
+            }
+        }
+        return newSentence.toString();
+    }
+
+    public String withRecursion(){
+        return replace(sentence,0);
+    }
+
+    public String replace(String sentence, int place){
+        Character currentCharacter = sentence.charAt(place);
+        if(changeMap.containsKey(Character.toLowerCase(currentCharacter)) || changeMap.containsKey(Character.toUpperCase(currentCharacter))){
+            currentCharacter = changeMap.get(Character.toLowerCase(currentCharacter));
+        }
+        sentence = sentence.substring(0,place)+currentCharacter+sentence.substring(place+1);
+
+        if(place<sentence.length()-1) {
+            sentence = replace(sentence,place+=1);
+        }
+        return sentence;
+    }
 }
--- src/test/java/io/zipcoder/Problem1Test.java
+++ src/test/java/io/zipcoder/Problem1Test.java
@@ -1,4 +1,36 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+
 public class Problem1Test {
+    Problem1 problem1;
+
+    @Before
+    public void setUp() {
+        HashMap<Character,Character> changeMap = new HashMap<Character, Character>();
+        changeMap.put('f', '7');
+        changeMap.put('s', '$');
+        changeMap.put('1', '!');
+        changeMap.put('a', '@');
+        problem1 = new Problem1("The Farmer went to the store to get 1 dollar’s worth of fertilizer",changeMap);
+
+    }
+
+    @Test
+    public void withLoop(){
+        String expected = "The 7@rmer went to the $tore to get ! doll@r’$ worth o7 7ertilizer";
+        String actual = problem1.changeWithLoop();
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void withRecursion(){
+        String expected = "The 7@rmer went to the $tore to get ! doll@r’$ worth o7 7ertilizer";
+        String actual = problem1.withRecursion();
+        Assert.assertEquals(expected,actual);
+    }
 }
