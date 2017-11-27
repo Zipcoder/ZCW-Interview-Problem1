59c59242d99871c79f7533522269c1e4f0c0ee04
merin j <merinj@zipcoders-MacBook-Pro-7.local>
2017-11-27 10:09:55 -0500

    Merin

--- src/test/java/io/zipcoder/Problem1Test.java
+++ src/test/java/io/zipcoder/Problem1Test.java
@@ -1,4 +1,21 @@
 package io.zipcoder;
 
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem1Test {
+    Problem1 problem1 = new Problem1();
+    String input = "The Farmer went to the store to get 1 dollar’s worth of fertilizer";
+    Map<Character, Character> map = new HashMap<Character, Character>();
+    @Test
+    public void changeStringTest(){
+
+    }
+    @Test
+    public void convertToMapValuesTest(){
+        problem1.convertToMapValues()
+    }
+
 }
