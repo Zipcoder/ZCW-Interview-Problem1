695a4bae17930c3b3de0c53080beeb4d538adaa1
raul <raul@zipcoders-MacBook-Pro.local>
2017-11-27 14:03:18 -0500

    raul

--- src/main/java/io/zipcoder/Problem1.java
+++ src/main/java/io/zipcoder/Problem1.java
@@ -15,31 +15,24 @@ public class Problem1 {
 
     public static void replaceChar() {
 
-        HashMap<Character, Integer> characterToReplace = new HashMap<Character, Integer>();
-
-        characterToReplace.put('f', 1);
-        characterToReplace.put('7', 2);
-        characterToReplace.put('s', 3);
-        characterToReplace.put('$', 4);
-        characterToReplace.put('1', 5);
-        characterToReplace.put('!', 6);
-        characterToReplace.put('a', 7);
-        characterToReplace.put('@', 8);
-
-
-        HashMap<String, String> stringConverted = new HashMap<String, String>();
-
+        HashMap<Character, Character> map = new HashMap<>();
+        // ‘f’ : ‘7’, ‘s’:’$’, ‘1’:’!’, ‘a’.:’@’
+        map.put('f', '7');
+        map.put('s', '$');
+        map.put('1', '!');
+        map.put('a', '@');
 
         String text = "The Farmer went to the store to get 1 dollar’s worth of fertilizer";
 
+        char[] splitString = text.toCharArray();
 
-        Map<String, String> result = Arrays.stream(text.split(" "))
-                .map(s -> s.split("="))
-                .collect(Collectors.toMap(
-                        a -> a[0],
-                        a -> a[1]
-                ));
+        for (int i = 0; i < splitString.length; i++) {
+            if (map.containsKey(splitString[i])) {
+                splitString[i] = map.get(splitString[i]);
+            }
+        }
 
+        System.out.println(new String(splitString));
 
 
     }
