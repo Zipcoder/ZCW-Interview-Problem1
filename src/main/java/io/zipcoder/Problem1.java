73e7991b24dae2c8c04ef4dbdaee7c14097afeb7
gabriela <gabriela@zipcoders-MacBook-Pro-6.local>
2017-11-27 10:04:45 -0500

    first 30 mins....

--- src/main/java/io/zipcoder/Problem1.java
+++ src/main/java/io/zipcoder/Problem1.java
@@ -1,4 +1,29 @@
 package io.zipcoder;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem1 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static String replaceThroughIteration(String input) {
+        Map<Character, Character> map = new HashMap<Character, Character>();
+        map.put('f','7');
+        map.put('s', '$');
+        map.put('1', '!');
+        map.put('a', '@');
+
+        for(Map.Entry<Character, Character> entry : map.entrySet()) {
+            //if a key in the map, matches a character in a string, replace with maps value for respective key
+            //have the new string returned
+        }
+    }
+
+    public String replaceThroughRecursion() {
+
+    }
+
 }
--- src/test/java/io/zipcoder/Problem1Test.java
+++ src/test/java/io/zipcoder/Problem1Test.java
@@ -1,4 +1,22 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem1Test {
+
+    @Test
+    public void replaceThroughIterationTest() {
+        String input = "The Farmer went to the store to get 1 dollar’s worth of fertilizer";
+        String expected = "The 7@rmer went to the $tore to get ! doll@r’$ worth of 7ertilizer";
+        String actual = replaceThroughIterationTest(input);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void replacedThroughRecursionTest() {
+
+    }
+
+
 }
