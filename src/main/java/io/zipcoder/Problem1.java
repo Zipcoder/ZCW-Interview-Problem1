0c696be2aca1dfcdbe823c05aacd31bcf5395980
ryan <ryan@zipcoders-MacBook-Pro-5.local>
2017-11-27 10:04:56 -0500

    ehhhhh

--- src/main/java/io/zipcoder/Problem1.java
+++ src/main/java/io/zipcoder/Problem1.java
@@ -1,4 +1,26 @@
 package io.zipcoder;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem1 {
+
+    //need a map
+    HashMap <Character, Character> characterMap = new HashMap<Character, Character>();
+
+    public String replaceCharacters(String stringToReplace){
+        characterMap.put('f','7');
+        characterMap.put('s', '$');
+        characterMap.put('1', '!');
+        characterMap.put('a', '@');
+
+        char[] charArray = stringToReplace.toCharArray();
+
+        for (int i = 0; i < charArray.length; i++){
+            if (charArray[i] == characterMap.)
+        }
+
+        return "";
+    }
+
 }
--- src/test/java/io/zipcoder/Problem1Test.java
+++ src/test/java/io/zipcoder/Problem1Test.java
@@ -1,4 +1,15 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem1Test {
+
+    @Test
+    public void replaceCharactersTest(){
+        String expected = "The 7@rmer went to the $tore to get ! doll@ar'$ worth of 7ertilizer";
+        String actual = "";
+        Assert.assertEquals(expected, actual);
+
+    }
 }
