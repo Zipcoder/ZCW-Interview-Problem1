839b34fcb7a51a0fadf622cd23aa6dd0a9992595
pavel <Pavelparfenov112@gmail.com>
2017-11-27 10:04:10 -0500

    "Iterate almost working"

--- src/main/java/io/zipcoder/Problem1.java
+++ src/main/java/io/zipcoder/Problem1.java
@@ -1,4 +1,52 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem1 {
+
+    private HashMap<Character, Character> cypher = new HashMap<Character, Character>(){{
+        put('f', '7');
+        put('s', '$');
+        put('1', '!');
+        put('a', '@');
+    }};
+
+    //step1 read character
+    //step2 decide if needs replacing
+    //step3 replace
+
+    Character getCharAt(String raw, int index){
+        return raw.charAt(index);
+    }
+
+    Boolean decideIfReplace(Character character){//assume .toLowerCase() has already been called on input String
+        Boolean output = false;
+        ArrayList<Character> wanted = new ArrayList<>(cypher.keySet());
+        for (Character wantChar :
+                wanted) {
+            if(character.equals(wantChar)) {
+                output = true;
+            }
+        }
+        return output;
+    }
+
+    String replaceChar(String raw, Character character, int index){
+        Character replacement = cypher.get(character);
+        return raw.replace(character, replacement);
+    }
+
+    public String encodeIterate(String raw){
+        String flattened = raw.toLowerCase();
+        String output = raw;
+        for (int i = 0; i < raw.length(); i++) {
+            Character character = getCharAt(flattened, i);
+            if (decideIfReplace(character)){
+                output = replaceChar(output, character, i);
+            }
+        }
+        return output;
+    }
 }
--- src/test/java/io/zipcoder/Problem1Test.java
+++ src/test/java/io/zipcoder/Problem1Test.java
@@ -1,4 +1,60 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem1Test {
+
+    @Test
+    public void getCharTest(){
+        Problem1 problem1 = new Problem1();
+        String raw = "The Farmer went to the store to get 1 dollar’s worth of fertilizer";
+
+        Character expected = 'F';
+        Character actual = problem1.getCharAt(raw, 4);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void decideIfReplaceTrueTest(){
+        Problem1 problem1 = new Problem1();
+        String raw = "the farmer went to the store to get 1 dollar’s worth of fertilizer";
+
+        Boolean actual = problem1.decideIfReplace(problem1.getCharAt(raw, 5));
+
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void decideIfReplaceFalseTest(){
+        Problem1 problem1 = new Problem1();
+        String raw = "the farmer went to the store to get 1 dollar’s worth of fertilizer";
+
+        Boolean actual = problem1.decideIfReplace(problem1.getCharAt(raw, 6));
+
+        Assert.assertFalse(actual);
+    }
+
+    @Test
+    public void replaceCharTest(){
+        Problem1 problem1 = new Problem1();
+        String raw = "the farmer went to the store to get 1 dollar’s worth of fertilizer";
+
+        String expected = "the 7armer went to the store to get 1 dollar’s worth o7 7ertilizer";
+        String actual = problem1.replaceChar(raw, 'f', 4);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void encodeIterateTest(){
+        Problem1 problem1 = new Problem1();
+        String raw = "The Farmer went to the store to get 1 dollar’s worth of fertilizer";
+
+        String expected = "The 7@rmer went to the $tore to get ! doll@r’$ worth of 7ertilizer";
+        String actual = problem1.encodeIterate(raw);
+
+        Assert.assertEquals(expected, actual);
+    }
 }
