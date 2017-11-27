1423322bffb67acf9eb6b6ec48ca334b96874f7b
linda <linda@zipcoders-MacBook-Pro-8.local>
2017-11-27 10:04:02 -0500

    first commit

---
+++ src/test/java/io/zipcoder/WordParserTest.java
@@ -0,0 +1,41 @@
+package io.zipcoder;
+
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.ArrayList;
+import static org.junit.Assert.*;
+
+public class WordParserTest {
+
+    private String nothing = "";
+    private String inputSentenceFinal =  "The Farmer went to the store to get 1 dollar’s worth of fertilizer";
+    private String inputSentenceTwoWords = "The Farmer";
+    private WordParser woodparser;
+
+//
+//    @Before
+//    public void setUp(){
+//
+//    }
+
+//    @Test
+//    public void parseFirstWordTest{
+//        woodparser = new WordParser();
+//        String expected = "The";
+//        String actual = woodparser.parseFirstWord(inputSentenceTwoWords);
+//        Assert.assertEquals(expected, actual);
+//    }
+
+
+
+
+
+
+
+
+}
+
+
+
