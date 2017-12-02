cfde0950fdd0c5e17fc338d7af5c9835a1afd742
andrea <andrea@zipcoders-MacBook-Pro-4.local>
2017-12-02 14:32:43 -0500

    re-did problem

--- src/main/java/io/zipcoder/Problem1.java
+++ src/main/java/io/zipcoder/Problem1.java
@@ -1,21 +1,29 @@
 package io.zipcoder;
 
 import java.util.HashMap;
-import java.util.Map;
 
 public class Problem1 {
 
-    String = "The Farmer went to the store to get 1 dollar's worth of fertilizer";
-
-    public static String iterationMethod(){
-        Map<Character, Character> newMap = new HashMap<Character, Character>();
-        for(Map.Entry<Character, Character> entry : map.entrySet())
-            newMap.put(entry.getKey().replace(), entry.getValue());
-        map = newMap;
-
+    public String iterationMethod(String string, HashMap<Character, Character> swapCharacter) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < string.length(); i++){
+            Character current = string.charAt(i);
+            if (swapCharacter.containsKey(Character.toLowerCase(current))) {
+                current = swapCharacter.get(Character.toLowerCase(current));
+            }
+            stringBuilder.append(current);
+        }
+        return stringBuilder.toString();
     }
 
-    public static String recursionMethod(){
-
+    public String recursionMethod(String string, HashMap<Character, Character> swapCharacter){
+        if(string.length() == 0) {
+            return "";
+        }
+        Character current = string.charAt(0);
+        if(swapCharacter.containsKey(Character.toLowerCase(current))){
+            current = swapCharacter.get(Character.toLowerCase(current));
+        }
+        return current + recursionMethod(string.substring(1), swapCharacter);
     }
 }
