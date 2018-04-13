1ab0f53d22a1e334ee342621854631ddc0077d39
Brian He <brianhe@zipcoders-MacBook-Pro-56.local>
2018-04-13 15:20:24 -0400

    iteration

--- src/main/java/io/zipcoder/Problem1.java
+++ src/main/java/io/zipcoder/Problem1.java
@@ -1,4 +1,15 @@
 package io.zipcoder;
 
+import java.util.HashMap;
+import java.util.Set;
+
 public class Problem1 {
+
+    public static String replaceWithIteration(HashMap<String, String> map, String input) {
+        Set<String> keys = map.keySet();
+        for(String key : keys) {
+            input = input.replaceAll(key, "\\" + map.get(key));
+        }
+        return input;
+    }
 }
--- src/test/java/io/zipcoder/Problem1Test.java
+++ src/test/java/io/zipcoder/Problem1Test.java
@@ -1,4 +1,42 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+
 public class Problem1Test {
+
+    @Test
+    public void replaceTest() {
+
+        HashMap<String, String> map = new HashMap<>();
+        map.put("f", "7");
+        map.put("s", "$");
+        map.put("1", "!");
+        map.put("a", "@");
+
+        String input = "the farmer went to the store to get 1 dollar’s worth of fertilizer";
+        String expected = "the 7@rmer went to the $tore to get ! doll@r’$ worth o7 7ertilizer";
+
+        String actual = Problem1.replaceWithIteration(map, input);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void replaceTest2() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("f", "7");
+        map.put("s", "$");
+        map.put("1", "!");
+        map.put("a", "@");
+
+        String input = "the farmer went to the store to get 1 dollar’s worth of fertilizer";
+        String expected = "f";
+
+        String actual = Problem1.replaceWithIteration(map, input);
+
+        Assert.assertEquals(expected, actual);
+    }
 }
